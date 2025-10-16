09a02835536eec620708806c7901c96315242b6a
YastMe <ale4xd@hotmail.com>
2025-10-16 12:54:01 +0200

    Fixed basic capacity calculation

--- scripts/hooks/rollData.mjs
+++ scripts/hooks/rollData.mjs
@@ -60,7 +60,7 @@ export function onGetRollData(doc, rollData) {
                     maxCapacity = 11;
                 }
 
-                if (rollData.akasha.maxCapacity !== undefined)
+                if (rollData.akasha.maxCapacity === undefined)
                     rollData.akasha.maxCapacity = maxCapacity;
                 else
                     rollData.akasha.maxCapacity += maxCapacity;
